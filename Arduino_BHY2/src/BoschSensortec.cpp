cdec47e2b288358b68ae7479ff2ef83cf8fa5047
Paolo Calao <paolo.calao@gmail.com>
2021-01-21 18:13:13 +0100

    Sensortec - implement logic using real bhy functions

--- Arduino_BHY2/src/BoschSensortec.cpp
+++ Arduino_BHY2/src/BoschSensortec.cpp
@@ -13,14 +13,48 @@ BoschSensortec::~BoschSensortec()
 
 void BoschSensortec::begin()
 {
-  bhy2_init(BHY2_SPI_INTERFACE, bhy2_spi_read, bhy2_spi_write, bhy2_delay_us, MAX_READ_WRITE_LEN, NULL, &_bhy2);
+  auto ret = bhy2_init(BHY2_SPI_INTERFACE, bhy2_spi_read, bhy2_spi_write, bhy2_delay_us, MAX_READ_WRITE_LEN, NULL, &_bhy2);
+  if (_debug) _debug->println(get_api_error(ret));
+
+  uint8_t stat;
+  ret = bhy2_get_boot_status(&stat, &_bhy2);
+  if (_debug) {
+    _debug->println(get_api_error(ret));
+    _debug->print("Boot status: ");
+    _debug->println(stat, HEX);
+  }
+
+  ret = bhy2_get_host_interrupt_ctrl(&stat, &_bhy2);
+  if (_debug) {
+    _debug->println(get_api_error(ret));
+    _debug->print("Interrupt ctrl: ");
+    _debug->println(stat, HEX);
+  }
+  ret = bhy2_get_host_intf_ctrl(&stat, &_bhy2);
+  if (_debug) {
+    _debug->println(get_api_error(ret));
+    _debug->print("Interface ctrl: ");
+    _debug->println(stat, HEX);
+  }
+
+  bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT, BoschParser::parseMetaEvent, NULL, &_bhy2);
+  bhy2_register_fifo_parse_callback(BHY2_SYS_ID_META_EVENT_WU, BoschParser::parseMetaEvent, NULL, &_bhy2);
+  bhy2_register_fifo_parse_callback(BHY2_SYS_ID_DEBUG_MSG, BoschParser::parseDebugMessage, NULL, &_bhy2);
+
+  ret = bhy2_get_and_process_fifo(_workBuffer, WORK_BUFFER_SIZE, &_bhy2);
+  if (_debug) _debug->println(get_api_error(ret));
+
+  for (uint8_t i = 1; i < BHY2_SENSOR_ID_MAX; i++) {
+    bhy2_register_fifo_parse_callback(i, BoschParser::parseData, NULL, &_bhy2);
+  }
+
+  bhy2_update_virtual_sensor_list(&_bhy2);
 }
 
 void BoschSensortec::configureSensor(SensorConfigurationPacket *config)
 {
-  bhy2_register_fifo_parse_callback(config->sensorId, BoschParser::parseData, NULL, &_bhy2);
-  bhy2_update_virtual_sensor_list(&_bhy2);
-  bhy2_set_virt_sensor_cfg(config->sensorId, config->sampleRate, config->latency, &_bhy2);
+  auto ret = bhy2_set_virt_sensor_cfg(config->sensorId, config->sampleRate, config->latency, &_bhy2);
+  if (_debug) _debug->println(get_api_error(ret));
 }
 
 uint8_t BoschSensortec::availableSensorData()
@@ -45,7 +79,8 @@ void BoschSensortec::addSensorData(const SensorDataPacket &sensorData)
 void BoschSensortec::update()
 {
   if (get_interrupt_status()) {
-    bhy2_get_and_process_fifo(_workBuffer, WORK_BUFFER_SIZE, &_bhy2);
+    auto ret = bhy2_get_and_process_fifo(_workBuffer, WORK_BUFFER_SIZE, &_bhy2);
+    if (_debug) _debug->println(get_api_error(ret));
   }
 }
 
