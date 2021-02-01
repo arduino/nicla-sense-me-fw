058043c5e068dea24eea91be9ca287e1ec30e668
Paolo Calao <paolo.calao@gmail.com>
2021-02-01 16:17:33 +0100

    bhy go tool: type alignment - use float32 instead of float64

--- tools/bhy-controller/src/sensor/sensor.go
+++ tools/bhy-controller/src/sensor/sensor.go
@@ -1,23 +1,24 @@
 package sensor
 
 import (
+	"encoding/binary"
 	"fmt"
 	"log"
-	"go.bug.st/serial"
-	"encoding/binary"
 	"math"
+
+	"go.bug.st/serial"
 )
 
-type SensorData struct{
-  id uint8
-  size uint8 
-  data uint64 
+type SensorData struct {
+	id   uint8
+	size uint8
+	data uint64
 }
 
-type SensorConfig struct{
-  id uint8
-  sampleRate float64 
-  latency uint32
+type SensorConfig struct {
+	id         uint8
+	sampleRate float32
+	latency    uint32
 }
 
 func errCheck(e error) {
@@ -26,7 +27,7 @@ func errCheck(e error) {
 	}
 }
 
-func openPort(usbPort string, baudRate int) (serial.Port) {
+func openPort(usbPort string, baudRate int) serial.Port {
 	mode := &serial.Mode{
 		BaudRate: baudRate,
 		Parity:   serial.NoParity,
@@ -68,7 +69,7 @@ func Read(usbPort string, baudRate int) {
 		if n != 10 {
 			fmt.Println("data not valid")
 		} else {
-			// Print the received sensor data 
+			// Print the received sensor data
 			var data SensorData
 			data.id = uint8(buff[0])
 			data.size = uint8(buff[1])
@@ -87,13 +88,13 @@ func Configure(usbPort string, baudRate int, id int, rate float64, latency int) 
 	// Fill a configuration struct just for clarity
 	var config SensorConfig
 	config.id = uint8(id)
-	config.sampleRate = rate
+	config.sampleRate = float32(rate)
 	config.latency = uint32(latency)
 	// Prepare configuration packet to send
 	opCode := []byte{3}
 	cId := []byte{config.id}
-	cSample := make([]byte, 8)
-	binary.LittleEndian.PutUint64(cSample[:], math.Float64bits(config.sampleRate))
+	cSample := make([]byte, 4)
+	binary.LittleEndian.PutUint32(cSample[:], math.Float32bits(config.sampleRate))
 	cLatency := make([]byte, 4)
 	binary.LittleEndian.PutUint32(cLatency, config.latency)
 	var packet []byte
@@ -106,6 +107,6 @@ func Configure(usbPort string, baudRate int, id int, rate float64, latency int) 
 	n, err := port.Write(packet)
 	errCheck(err)
 	fmt.Printf("Sent %v bytes\n", n)
-	
+
 	port.Close()
 }
