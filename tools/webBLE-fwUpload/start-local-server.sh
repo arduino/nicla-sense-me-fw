803fd0e35343bfc620f80c76d125fe7c80577ca9
Paolo Calao <paolo.calao@gmail.com>
2021-02-05 18:37:20 +0100

    WebApp: introduce script to start a local server
    This is needed for importing json files into the html pages

---
+++ tools/webBLE-fwUpload/start-local-server.sh
@@ -0,0 +1,5 @@
+# If Python version returned above is 3.X
+#python3 -m http.server
+# On windows try "python" instead of "python3", or "py -3"
+# If Python version returned above is 2.X
+python -m SimpleHTTPServer
