ce6898adccad9a04b6683124335dba118545957d
Paolo Calao <paolo.calao@gmail.com>
2021-02-09 18:56:15 +0100

    Arduino_BHY2: update interpreter example using new parse functions

--- Arduino_BHY2/examples/interpreter/interpreter.ino
+++ Arduino_BHY2/examples/interpreter/interpreter.ino
@@ -1,59 +1,75 @@
 #include "Arduino.h"
 #include "Arduino_BHY2.h"
-#include "interpreter.h"
+
+#define ACCEL_ID (4)
+DataXYZ accValue;
+
+#define GYRO_ID (13)
+DataXYZ gyroValue;
+
+#define ORI_ID (43)
+DataOrientation orientation;
+
+void configureSensors() {
+  SensorConfigurationPacket config;
+  config.sampleRate = 1;
+  config.latency = 0;
+
+  config.sensorId = ACCEL_ID;
+  BHY2.configureSensor(&config);
+
+  config.sensorId = ORI_ID;
+  BHY2.configureSensor(&config);
+
+  config.sensorId = GYRO_ID;
+  BHY2.configureSensor(&config);
+
+  config.sensorId = 34;
+  BHY2.configureSensor(&config);
+}
 
 void setup()
 {
   Serial.begin(115200);
 
-  //BHY2.debug(Serial);
   BHY2.begin();
 
-  // Configure the gyroscope
-  SensorConfigurationPacket config;
-  config.sampleRate = 1;
-  config.latency = 0;
-  config.sensorId = 13;
-  BHY2.configureSensor(&config);
-}
-
-int16_t extractInt16(uint64_t data, uint8_t offset) {
-  uint16_t low = (data >> (offset * 8)) & 0x00FF;
-  uint16_t high = (data >> ((offset + 1) * 8) ) & 0x0FF;
-  int16_t value = (int16_t) (low | (high << 8));
-  return value;
-}
-
-void parseXYZ(uint64_t data, DataXYZ& vector)
-{
-  vector.x = extractInt16(data, 0);
-  vector.y = extractInt16(data, 2);
-  vector.z = extractInt16(data, 4);
-  Serial.print("value: ");
-  Serial.print("x: ");
-  Serial.print(vector.x);
-  Serial.print("   y: ");
-  Serial.print(vector.y);
-  Serial.print("   z: ");
-  Serial.println(vector.z);
+  configureSensors();
 }
 
 void loop()
 {
-  static auto time = millis();
-  if (millis() - time >= 200) {
-    time = millis();
+  static auto printTime = millis();
+  static auto sampleTime = millis();
+
+  if (millis() - sampleTime >= 10) {
+    sampleTime = millis();
 
     if (BHY2.availableSensorData() > 0) {
       SensorDataPacket data;
       BHY2.readSensorData(data);
 
-      Serial.println("received data from sensor: ");
-      Serial.println(data.sensorId);
-      DataXYZ value;
-      parseXYZ(data.data, value);
-      Serial.println();
+      if (data.sensorId == ACCEL_ID) {
+        Serial.print("Received accel: ");
+        BHY2.parse(data, accValue);
+
+      } else if (data.sensorId == GYRO_ID) {
+        Serial.print("Received gyro: ");
+        BHY2.parse(data, gyroValue);
+
+      } else if (data.sensorId == ORI_ID) {
+        Serial.println("orientation");
+        BHY2.parse(data, orientation, 360.0f / 32768.0f);
+
+      }
     }
+  }
+
+  if (millis() - printTime >= 200) {
+    printTime = millis();
+
+    Serial.println(orientation.toString());
+    Serial.println(gyroValue.toString());
   }
 
   BHY2.update();
