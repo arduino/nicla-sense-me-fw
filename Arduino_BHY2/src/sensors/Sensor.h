fe40b561667e7863e3aa0a7c590dce3fdbaf3bd8
Paolo Calao <paolo.calao@gmail.com>
2021-05-28 11:01:18 +0200

    Clean-up sensors

--- Arduino_BHY2/src/sensors/Sensor.h
+++ Arduino_BHY2/src/sensors/Sensor.h
@@ -11,13 +11,10 @@ public:
   Sensor(uint8_t id);
   virtual ~Sensor();
 
-  //void init(uint8_t id);
   uint8_t id();
-  //void configure(uint8_t id, float rate, uint32_t latency);
   void configure(float rate, uint32_t latency);
   void disable();
 
-  // void setCallback();
   virtual void setData(SensorDataPacket &data) = 0;
   virtual String toString() = 0;
 
--- Arduino_BHY2_HOST/src/sensors/Sensor.cpp
+++ Arduino_BHY2_HOST/src/sensors/Sensor.cpp
@@ -24,19 +24,6 @@ uint8_t Sensor::id()
   return _id;
 }
 
-//void Sensor::init(uint8_t id)
-//{
-  //_id = id;
-  //// register sensor
-  //sensorManager.subscribe(this);
-//}
-
-//void Sensor::configure(uint8_t id, float rate, uint32_t latency)
-//{
-  //_id = id;
-  //configure(rate, latency);
-//}
-
 void Sensor::configure(float rate, uint32_t latency)
 {
   SensorConfigurationPacket config {_id, rate, latency};
