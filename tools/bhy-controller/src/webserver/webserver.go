6b467522b13bb966f31733c9ce015f58da9aaab1
Silvano Cerza <silvanocerza@gmail.com>
2022-03-15 10:26:10 +0100

    Webserver static file are now embedded in the bhy binary

--- tools/bhy-controller/src/webserver/webserver.go
+++ tools/bhy-controller/src/webserver/webserver.go
@@ -1,12 +1,21 @@
 package webserver
 
 import (
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/pkg/browser"
 )
+
+// content holds all the static file necessary
+// to serve our local webserver.
+// Since we embed them in the final binary we won't need to
+// distribute the static file.
+//go:embed *.html
+//go:embed *.json
+var content embed.FS
 
 func errCheck(e error) {
 	if e != nil {
@@ -18,7 +27,7 @@ func startServer() {
 	fmt.Printf("Starting server at port 8000\n")
 	fmt.Printf("Visit webpage at address http://localhost:8000/index.html\n")
 
-	fileServer := http.FileServer(http.Dir("./webserver/"))
+	fileServer := http.FileServer(http.FS(content))
 	http.Handle("/", fileServer)
 	http.ListenAndServe(":8000", nil)
 }
