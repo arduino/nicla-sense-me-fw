a90cc0023ae8632006255da230cd6bfd435c54ab
Silvano Cerza <silvanocerza@gmail.com>
2022-03-17 11:18:00 +0100

    Update hardcoded paths to static file to use embedded filesystem

--- tools/bhy-controller/src/sensor/parser.go
+++ tools/bhy-controller/src/sensor/parser.go
@@ -1,15 +1,13 @@
 package sensor
 
 import (
+	"arduino/bhy/static"
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"path"
-	"path/filepath"
-	"strconv"
 	"math"
+	"strconv"
 )
 
 type SensorType struct {
@@ -29,12 +27,8 @@ var scheme map[string]interface{}
 var types map[string]interface{}
 
 func loadScheme() map[string]interface{} {
-	// Retrieve directory of bhy executable to look for json files
-	exe, err := os.Executable()
-	errCheck(err)
-	exePath := filepath.Dir(exe)
-
-	jsonFile, err := os.Open(path.Join(exePath, "/webserver/parse-scheme.json"))
+	// jsonFile, err := os.Open(path.Join(exePath, "/webserver/parse-scheme.json"))
+	jsonFile, err := static.Content.Open("parse-scheme.json")
 	errCheck(err)
 	defer jsonFile.Close()
 
@@ -47,12 +41,7 @@ func loadScheme() map[string]interface{} {
 }
 
 func loadTypes() map[string]interface{} {
-	// Retrieve directory of bhy executable to look for json files
-	exe, err := os.Executable()
-	errCheck(err)
-	exePath := filepath.Dir(exe)
-
-	jsonFile, err := os.Open(path.Join(exePath, "/webserver/sensor-type-map.json"))
+	jsonFile, err := static.Content.Open("sensor-type-map.json")
 	errCheck(err)
 	defer jsonFile.Close()
 
@@ -76,24 +65,24 @@ func parseData(data *SensorData) {
 
 	typeMap := scheme["types"].([]interface{})
 	//Map the right type:
-  if (sensorScheme == "quaternion") {
+	if sensorScheme == "quaternion" {
 		typeScheme = typeMap[0].(map[string]interface{})
-  } else if (sensorScheme == "xyz") {
+	} else if sensorScheme == "xyz" {
 		typeScheme = typeMap[1].(map[string]interface{})
-  } else if (sensorScheme == "orientation") {
+	} else if sensorScheme == "orientation" {
 		typeScheme = typeMap[2].(map[string]interface{})
-  } else if (sensorScheme == "event") {
-    eventcount = sensorMap["eventcount"].(float64)
+	} else if sensorScheme == "event" {
+		eventcount = sensorMap["eventcount"].(float64)
 		typeScheme = typeMap[3].(map[string]interface{})
-  } else if (sensorScheme == "activity") {
+	} else if sensorScheme == "activity" {
 		typeScheme = typeMap[4].(map[string]interface{})
-  } else if (sensorScheme == "BSECOutput") {
+	} else if sensorScheme == "BSECOutput" {
 		typeScheme = typeMap[5].(map[string]interface{})
-  } else if (sensorScheme == "BSECOutputV2") {
+	} else if sensorScheme == "BSECOutputV2" {
 		typeScheme = typeMap[6].(map[string]interface{})
-  } else if (sensorScheme == "BSECOutputV2Full") {
+	} else if sensorScheme == "BSECOutputV2Full" {
 		typeScheme = typeMap[6].(map[string]interface{})
-  }
+	}
 
 	fields := typeScheme["parse-scheme"].([]interface{})
 
@@ -107,47 +96,47 @@ func parseData(data *SensorData) {
 		fieldFactor := float32(field["scale-factor"].(float64))
 		var fieldSize int = 0
 		var value float32 = 0
-		var activityVal uint16 = 0;
+		var activityVal uint16 = 0
 
 		if fieldType == "uint8" {
 			value = float32(uint8(data.data[index])) * fieldFactor
 			fieldSize = 1
 		} else if fieldType == "int8" {
-      value = float32(int8(data.data[index])) * fieldFactor; 
-      fieldSize = 1;
-    } else if fieldType == "uint16" {
+			value = float32(int8(data.data[index])) * fieldFactor
+			fieldSize = 1
+		} else if fieldType == "uint16" {
 			if sensorScheme == "activity" {
-				activityVal = binary.LittleEndian.Uint16(data.data[index:index+2])
+				activityVal = binary.LittleEndian.Uint16(data.data[index : index+2])
 			} else {
 				value = float32(uint16(binary.LittleEndian.Uint16(data.data[index:index+2]))) * fieldFactor
 			}
 			fieldSize = 2
-    } else if fieldType == "int16" {
+		} else if fieldType == "int16" {
 			value = float32(int16(binary.LittleEndian.Uint16(data.data[index:index+2]))) * fieldFactor
 			fieldSize = 2
-    } else if fieldType == "uint24" {
+		} else if fieldType == "uint24" {
 			temp := []byte{0, 0, 0, 0}
 			copy(temp[0:3], data.data[index:index+3])
 			value = float32(binary.LittleEndian.Uint32(temp)) * fieldFactor
 			fieldSize = 3
 		} else if fieldType == "uint32" {
 			value = float32(uint32(binary.LittleEndian.Uint32(data.data[index:index+4]))) * fieldFactor
-      fieldSize = 4;
-    } else if fieldType == "float" {
+			fieldSize = 4
+		} else if fieldType == "float" {
 			value = float32(math.Float32frombits(binary.LittleEndian.Uint32(data.data[index:index+4]))) * fieldFactor
 			fieldSize = 4
 		} else if fieldType == "none" {
-      eventcount = eventcount + 1
-      sensorMap["eventcount"] = eventcount
-      fieldSize = 0;
-    } else {
+			eventcount = eventcount + 1
+			sensorMap["eventcount"] = eventcount
+			fieldSize = 0
+		} else {
 			panic("Unknown sensor type")
 		}
 
-    if sensorScheme == "activity" {
-      act := geActivityString(activityVal)
+		if sensorScheme == "activity" {
+			act := geActivityString(activityVal)
 			fmt.Printf(" %s : %s  ", fieldName, act)
-    } else if sensorScheme == "event" {
+		} else if sensorScheme == "event" {
 			index += fieldSize
 			fmt.Printf(" %s : %d  ", fieldName, int(eventcount))
 		} else {
@@ -161,34 +150,33 @@ func parseData(data *SensorData) {
 
 }
 
-
 func geActivityString(activityVal uint16) string {
 
 	activityMessages := [16]string{
-                            "Still activity ended",
-                            "Walking activity ended",
-                            "Running activity ended",
-                            "On bicycle activity ended",
-                            "In vehicle activity ended",
-                            "Tilting activity ended",
-                            "In vehicle still ended",
-                            "",
-                            "Still activity started",
-                            "Walking activity started",
-                            "Running activity started",
-                            "On bicycle activity started",
-                            "In vehicle activity started",
-                            "Tilting activity started",
-                            "In vehicle still started",
-                            ""}
+		"Still activity ended",
+		"Walking activity ended",
+		"Running activity ended",
+		"On bicycle activity ended",
+		"In vehicle activity ended",
+		"Tilting activity ended",
+		"In vehicle still ended",
+		"",
+		"Still activity started",
+		"Walking activity started",
+		"Running activity started",
+		"On bicycle activity started",
+		"In vehicle activity started",
+		"Tilting activity started",
+		"In vehicle still started",
+		""}
 
-	i := 0;
-  for i = 0; i < 16; i++ {
-    maskedVal := (activityVal & (0x0001 << i)) >> i
-    if (maskedVal == 1) {
+	i := 0
+	for i = 0; i < 16; i++ {
+		maskedVal := (activityVal & (0x0001 << i)) >> i
+		if maskedVal == 1 {
 			break
-    }
-  }
+		}
+	}
 
 	return activityMessages[i]
 }
