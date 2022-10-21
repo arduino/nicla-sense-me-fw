1869adbadf6843efb8ee3fac0c9e33a89eb42f01
zg guo <lucas.guo.cn@gmail.com>
2022-10-20 22:14:34 -0700

    add sketch that need low delays in sending sensor data especially over BLE

---
+++ Arduino_BHY2/examples/AppLowDelay/AppLowDelay.ino
@@ -0,0 +1,34 @@
+/* 
+ * Use this sketch if you want to control nicla from 
+ * an external device acting as a host.
+ * Here, nicla just reacts to external stimuli coming from
+ * the eslov port or through BLE 
+ * 
+ * NOTE: Remember to choose your Nicla configuration! 
+ * If Nicla is used as a Shield, provide the NICLA_AS_SHIELD parameter.
+ * If you want to enable just one between I2C and BLE,
+ * use NICLA_I2C or NICLA_BLE parameters.
+ *
+*/
+
+#include "Arduino.h"
+#include "Arduino_BHY2.h"
+
+// Set DEBUG to true in order to enable debug print
+#define DEBUG false
+
+void setup()
+{
+#if DEBUG
+  Serial.begin(115200);
+  BHY2.debug(Serial);
+#endif
+
+  BHY2.begin();
+}
+
+void loop()
+{
+  // Update and then sleep only for 1ms
+  BHY2.update(1);
+}
